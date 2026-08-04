f56004fbaac268d0540ee6f0fec81fad9358f4be
mzlei <oilinsea@163.com>
2026-08-04 22:08:56 +0800

    1、重载感度试验的漏泄量取整个保压周期的 2、修改重载运用取保压标准时间的错误

--- app/src/main/java/com/thnet/tailairbrakingtest/testwind/Test_GD_ZZYY.java
+++ app/src/main/java/com/thnet/tailairbrakingtest/testwind/Test_GD_ZZYY.java
@@ -3,8 +3,18 @@ package com.thnet.tailairbrakingtest.testwind;
 ///重载运用的感度试验
 public class Test_GD_ZZYY extends Test_GD{
     public Test_GD_ZZYY(){
-        standardLeak = SysParamsAll.get_zzyygdStandardLeak();//漏泄量标准
+        standardKeepTime = SysParamsAll.get_zzyygdStandardTime();//保压时间标准
         standardDrop = SysParamsAll.get_zzyygdStandardDrop();
         standardLeak = SysParamsAll.get_zzyygdStandardLeak();
+    }
+
+    //重载运用的感度试验，漏泄量取整个保压时间内的漏泄量，而不是第一分钟
+    @Override
+    protected void updateTestLeakValue(int testLeakValue) {
+        int lxl = testLeakValue;
+        if (lxl < 0) {
+            lxl = 0;
+        }
+        setTestLeakValue(lxl);
     }
 }
--- app/src/main/java/com/thnet/tailairbrakingtest/testwind/Test_GD_ZZZB.java
+++ app/src/main/java/com/thnet/tailairbrakingtest/testwind/Test_GD_ZZZB.java
@@ -7,4 +7,14 @@ public class Test_GD_ZZZB extends Test_GD{
         standardDrop = SysParamsAll.get_zzzbgdStandardDrop();
         standardLeak = SysParamsAll.get_zzzbgdStandardLeak();
     }
+
+    //重载整备的感度试验，漏泄量取整个保压时间内的漏泄量，而不是第一分钟
+    @Override
+    protected void updateTestLeakValue(int testLeakValue) {
+        int lxl = testLeakValue;
+        if (lxl < 0) {
+            lxl = 0;
+        }
+        setTestLeakValue(lxl);
+    }
 }
